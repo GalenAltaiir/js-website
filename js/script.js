84fafa712db0522ee2de6ae3eb926357b7d8ef08
Student <student@lon-lab2-12.navitas.local>
2022-01-19 16:10:18 +0000

    simple js website

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function help(){
+    document.getElementById("helping").innerHTML = "Thank You For Wanting To Help"
+    document.getElementById("cta").style.backgroundColor = "green"
+    document.getElementById("btn-help").innerHTML = "I HELPED"
+}
+
+function msg(){
+    alert("Thank You For Donating!");
+}
